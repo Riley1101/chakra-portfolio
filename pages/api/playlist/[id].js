f440115cf860152c71cf254ed4fbe4729a79c07c
Riley1101 <www.riley.s.eileen@gmail.com>
2022-08-16 15:08:53 +0630

    update on api

--- pages/api/playlist/[id].js
+++ pages/api/playlist/[id].js
@@ -1,20 +1,25 @@
 export default async function handler(req, res) {
-  let {
-    query: { id },
-  } = req;
-  let key = process.env.NEXT_PUBLIC_YOUTUBE;
-  console.log(id);
-  let response = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/playlistItems?part=id&part=snippet&playlistId=${id}&key=${key}`,
-    {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_YOUTUBE}`,
+  try {
+    let {
+      query: { id },
+    } = req;
+    let key = process.env.NEXT_PUBLIC_YOUTUBE;
+    let response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=id&part=snippet&playlistId=${id}&key=${key}`,
+      {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_YOUTUBE}`,
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => res);
+    if (response) {
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({
+        message: "Error with api credentials",
+      });
     }
-  )
-    .then((res) => res.json())
-    .then((res) => res);
-  if (response) {
-    res.status(200).json(response);
-  } else {
+  } catch {
     res.status(404).json({
       message: "Error with api credentials",
     });
